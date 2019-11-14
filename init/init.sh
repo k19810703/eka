360e9afe18fe84fb511183f28dedbd31e8ff8508
wuhd <k19810703@163.com>
2019-11-14 23:11:10 +0800

    edit

--- init/init.sh
+++ init/init.sh
@@ -8,7 +8,7 @@ curl apmserver:8200
 #   -H 'kbn-version: 7.4.2' \
 #   -d '{"attributes": {"title": "app-service1-*","timeFieldName": "@timestamp","fields": "[]"}}'
 # curl -X POST \
-#   http://localhost:5601/api/saved_objects/index-pattern \
+#   http://kibna:5601/api/saved_objects/index-pattern \
 #   -H 'Content-Type: application/json' \
 #   -H 'kbn-version: 7.4.2' \
 #   -d '{"attributes": {"title": "app-service2-*","timeFieldName": "@timestamp","fields": "[]"}}'
