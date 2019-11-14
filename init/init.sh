972c6968999a00df1b03754214762625ebcf4ef2
wuhd <k19810703@163.com>
2019-11-14 23:09:09 +0800

    edit

--- init/init.sh
+++ init/init.sh
@@ -1,13 +1,14 @@
-curl localhost:9200/_cat/health
-curl localhost:5601/api/status
+curl elasticsearch:9200/_cat/health
+curl kibana:5601/api/status
+curl apmserver:8200
 
-curl -X POST \
-  http://localhost:5601/api/saved_objects/index-pattern \
-  -H 'Content-Type: application/json' \
-  -H 'kbn-version: 7.4.2' \
-  -d '{"attributes": {"title": "app-service1-*","timeFieldName": "@timestamp","fields": "[]"}}'
-curl -X POST \
-  http://localhost:5601/api/saved_objects/index-pattern \
-  -H 'Content-Type: application/json' \
-  -H 'kbn-version: 7.4.2' \
-  -d '{"attributes": {"title": "app-service2-*","timeFieldName": "@timestamp","fields": "[]"}}'
+# curl -X POST \
+#   http://kibna:5601/api/saved_objects/index-pattern \
+#   -H 'Content-Type: application/json' \
+#   -H 'kbn-version: 7.4.2' \
+#   -d '{"attributes": {"title": "app-service1-*","timeFieldName": "@timestamp","fields": "[]"}}'
+# curl -X POST \
+#   http://localhost:5601/api/saved_objects/index-pattern \
+#   -H 'Content-Type: application/json' \
+#   -H 'kbn-version: 7.4.2' \
+#   -d '{"attributes": {"title": "app-service2-*","timeFieldName": "@timestamp","fields": "[]"}}'
